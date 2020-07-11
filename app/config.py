04e6e2dc40f5f674b7085f532f2f2b6eed09e732
satyam <satyam.chauhan@squareyards.co.in>
2020-07-11 10:19:51 +0530

    dynamic products YAML data set in database folder and changes in CSS

--- app/config.py
+++ app/config.py
@@ -1,5 +1,10 @@
-import datetime, os
+import datetime, os, yaml
 
 class Config:
     def __init__(self):
-        print('Config file initialize')
+        self.dataBasePath = '{}/app/Database'.format(os.getcwd())
+
+    def productData(self):
+        productYamlfile = '{}/products.yaml'.format(self.dataBasePath)
+        productsData = yaml.load(open(productYamlfile), Loader=yaml.FullLoader)
+        return productsData
