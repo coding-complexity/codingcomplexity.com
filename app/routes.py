3e1ecf52ebf694326501b0b4f5a00f098084eacb
satyam <satyam.chauhan@squareyards.co.in>
2020-08-07 01:22:45 +0530

    Complete About Us Page

--- app/routes.py
+++ app/routes.py
@@ -8,10 +8,14 @@ from app.controller.product import Product
 def index():
     home = Home()
     homeData = home.index()
-    return render_template('home/index.html', title='Coding Complexity LLC - Home Page', homeData=homeData)
+    return render_template('home/index.html', title='Home - Coding Complexity LLC', homeData=homeData)
 
 @app.route('/product/<productSlug>')
 def product(productSlug):
     product = Product()
     productDetails = product.index(productSlug)
     return render_template('product/product.html', title=productDetails['pageTitle'], productDetails=productDetails)
+
+@app.route('/about-us')
+def aboutUs():
+    return render_template('static/aboutUs.html', title='About Us - Coding Complexity LLC')
